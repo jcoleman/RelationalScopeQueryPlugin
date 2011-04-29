b5934bca7f4342fd060349ae04ebddf6f85aa7d8
James Coleman <jtc331@gmail.com>
2011-04-29 10:27:40 -0400

    Relscope is officially going version 2.0 soon!

--- RelationallyScopedQueryGrailsPlugin.groovy
+++ RelationallyScopedQueryGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import com.radiadesign.relationalscope.RelationalScope
 
 class RelationallyScopedQueryGrailsPlugin {
     // the plugin version
-    def version = "1.0"
+    def version = "2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
