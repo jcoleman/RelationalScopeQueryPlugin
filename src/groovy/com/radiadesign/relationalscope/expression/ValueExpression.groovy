154dd1abd0068df8dd9afb8ac24b37253a5c5a8d
James Coleman <jtc331@gmail.com>
2011-04-29 08:42:56 -0400

    Remove old import.

--- src/groovy/com/radiadesign/relationalscope/expression/ValueExpression.groovy
+++ src/groovy/com/radiadesign/relationalscope/expression/ValueExpression.groovy
@@ -3,8 +3,6 @@ package com.radiadesign.relationalscope.expression
 import com.radiadesign.relationalscope.RelationalScopeBuilder
 import com.radiadesign.relationalscope.RelationalScope
 
-import  org.hibernate.type.TypeFactory
-
 class ValueExpression extends ExpressionBase {
   
   def value
